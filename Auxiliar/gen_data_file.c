c00ab57cab11b4d967960345b2011974768f9844
Dennis Gonçalves <camaradadennis@gmail.com>
2024-08-06 09:10:26 -0300

    Mudanças no programa auxiliar de geração de um arquivo de dados. 1) Não é necessário gerar os arquivos de novos carregamentos, porque já temos estes no exercício. 2) Incluído um Makefile para compilação do mesmo.

--- Auxiliar/gen_data_file.c
+++ Auxiliar/gen_data_file.c
@@ -6,10 +6,8 @@
 #include <time.h>
 
 #define DB_FILE "GraosRecebidos-2024.dat"
-#define NOVO_FILE "1_Novo_Carregamento"
 
 #define N_CARREGAMENTOS 250
-#define N_AMOSTRAS 25
 
 #define _cleanup_(f) __attribute((cleanup(f)))
 
@@ -27,29 +25,19 @@ void
 gen_carga(Carga *carga)
 {
     carga->origem = rand() % 5 + 1; // só cinco origens diferentes
-    carga->cod_carga = rand() % 1000 + 1;
-    carga->mes_receb = rand() % 12 + 1;
-    carga->dia_receb = rand() % 31 + 1;
-    carga->tp_transgenico = rand() % 2;
+    carga->cod_carga = rand() % 1000 + 1; // valores entre 1 e 999
+    carga->mes_receb = rand() % 12 + 1; // número de meses
+    carga->dia_receb = rand() % 31 + 1; // número de dias
+    carga->tp_transgenico = rand() % 2; // apenas 0 e 1 permitidos
     carga->peso_bruto_t = (float) (rand() % 250 + 50) * 0.1;
     carga->perc_impurezas = (float) (rand() % 100 + 10) * 0.1;
     carga->perc_umidade = (float) (rand() % 300 + 10) * 0.1;
-}
-
-void
-gen_amostra(Amostra *amostra, int id)
-{
-    amostra->ident = id;
-    amostra->peso_bruto_kg = (float) (rand() % 50 + 10) * 0.1;
-    amostra->peso_impurezas_g = rand() % 100 + 10;
-    amostra->grau_umidade = (float) (rand() % 300 + 10) * 0.1;
 }
 
 int
 main()
 {
     _cleanup_(cleanup_free) Carga *carga;
-    _cleanup_(cleanup_free) Amostra *amostra;
     FILE *fp;
 
     srand(time(0));
@@ -66,29 +54,6 @@ main()
         fatal("Erro de abertura do arquivo");
 
     fwrite(carga, sizeof(Carga), N_CARREGAMENTOS, fp);
-
-    if (fclose(fp))
-        fatal("Erro de fechamento do arquivo");
-
-    amostra = malloc(N_AMOSTRAS * sizeof(Amostra));
-    if (!amostra)
-        fatal("Erro de alocação de memória");
-
-    for (int i = 0; i < N_AMOSTRAS; ++i)
-        gen_amostra(&amostra[i], i + 1);
-
-    fp = fopen(NOVO_FILE, "w");
-    if (!fp)
-        fatal("Erro de abertura do arquivo");
-
-    fprintf(fp, "%d %d %.1f %d %d\n", 123, 88, 20.5, N_AMOSTRAS, 0);
-
-    for (int i = 0; i < N_AMOSTRAS; ++i)
-    {
-        fprintf(fp, "%d %.1f %d %.1f\n",
-            amostra[i].ident, amostra[i].peso_bruto_kg,
-            amostra[i].peso_impurezas_g, amostra[i].grau_umidade);
-    }
 
     if (fclose(fp))
         fatal("Erro de fechamento do arquivo");
